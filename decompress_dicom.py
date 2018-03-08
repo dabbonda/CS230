f75c3def9810f96caa701806812bd8bf5da850bf
David Xue <dxue@stanford.edu>
2018-03-08 11:12:23 -0800

    Update decompress dicoms

--- decompress_dicom.py
+++ decompress_dicom.py
@@ -5,62 +5,70 @@ Decompresses .dcm files within the Fetal directory.
 import gdcm
 import sys
 import os
+from pprint import pprint
 
-# change to FetalLungNormal and get rid of the /patient part in both areas if trying to decompress the normal ones
-patients = os.listdir("/data/FetalLung/FetalLungAbnormal/patient")
-# patients = os.listdir("/data/FetalLung/FetalLungNormal")
-for folder in patients:
-    read_dir = "/data/FetalLung/FetalLungAbnormal/patient/" + folder + "/COR SSFSE BODY/"
-    # read_dir = "/data/FetalLung/FetalLung/" + folder + "/COR SSFSE BODY/"
-    try:
-        os.chdir(read_dir)
-    except: # for some reason some of the patients have ssfsex instead?
+
+
+# reads from /data/FetalLung and writes to /home/mazin/FetalLung
+def decompress_file(original_file):
+    file_path, ext = original_file.split('.')
+    decompressed_file = '-d.'.join([file_path, ext])
+    decompressed_file = decompressed_file.replace('data', 'home/mazin')
+
+    r = gdcm.ImageReader()
+    r.SetFileName(original_file)
+    if not r.Read():
+        print("error occurred, one file was unreadable: {}".format(original_file))
+        unprocessed.append(tuple((original_file, 'READ')))
+        return
+
+
+    ir = r.GetImage()
+    w = gdcm.ImageWriter()
+    image = w.GetImage()
+
+    image.SetNumberOfDimensions(ir.GetNumberOfDimensions())
+    dims = ir.GetDimensions()
+
+
+    image.SetDimension(0, ir.GetDimension(0))
+    image.SetDimension(1, ir.GetDimension(1))
+
+    pixeltype = ir.GetPixelFormat()
+    image.SetPixelFormat(pixeltype)
+
+    pi = ir.GetPhotometricInterpretation()
+    image.SetPhotometricInterpretation(pi)
+
+    pixeldata = gdcm.DataElement(gdcm.Tag(0x7fe0, 0x0010))
+    str1 = ir.GetBuffer()
+    # print ir.GetBufferLength()
+    pixeldata.SetByteValue(str1, gdcm.VL(len(str1)))
+    image.SetDataElement(pixeldata)
+
+#     print(decompressed_file)
+#     w.SetFileName('/home/mazin/temp.dcm')
+    w.SetFileName(decompressed_file)
+    w.SetFile(r.GetFile())
+    w.SetImage(image)
+    if not w.Write():
+        print("error occurred, one file was unwritable: {}".format(original_file))
+        unprocessed.append(tuple((original_file, 'WRITE')))
+        return
+
+unprocessed = []
+# for root, subdirs, files in os.walk(os.path.join(os.getenv('HOME'), 'FetalLung')):
+for root, subdirs, files in os.walk('/data/FetalLung'):
+    print(root)
+    if not files:
         continue
-    dicomms = os.listdir(os.getcwd())
-    for filename in dicomms:
-        if filename.endswith(".dcm"):
-            # print(os.path.join(directory, filename))
-            os.chdir(read_dir)# have to change back since we changed to write
-            file1 = filename
-            file2 = filename.split(".")[0] + "-d.dcm"
+    for file in files:
+        if not file.endswith('.dcm'):
+            continue
+        
+        file_path = os.path.join(root,file)
+        decompress_file(file_path)
 
-            r = gdcm.ImageReader()
-            r.SetFileName(file1)
-            if not r.Read():
-                print "error occurred, one file was unreadable"
-                sys.exit(1)
-
-            os.chdir(os.getenv("HOME"))
-
-            ir = r.GetImage()
-            w = gdcm.ImageWriter()
-            image = w.GetImage()
-
-            image.SetNumberOfDimensions(ir.GetNumberOfDimensions())
-            dims = ir.GetDimensions()
-
-
-            image.SetDimension(0, ir.GetDimension(0))
-            image.SetDimension(1, ir.GetDimension(1))
-
-            pixeltype = ir.GetPixelFormat()
-            image.SetPixelFormat(pixeltype)
-
-            pi = ir.GetPhotometricInterpretation()
-            image.SetPhotometricInterpretation(pi)
-
-            pixeldata = gdcm.DataElement(gdcm.Tag(0x7fe0, 0x0010))
-            str1 = ir.GetBuffer()
-            # print ir.GetBufferLength()
-            pixeldata.SetByteValue(str1, gdcm.VL(len(str1)))
-            image.SetDataElement(pixeldata)
-
-            w.SetFileName(file2)
-            w.SetFile(r.GetFile())
-            w.SetImage(image)
-            if not w.Write():
-                sys.exit(1)
-        else:
-            pass
-os.chdir(os.getenv("HOME"))
-os.system("rm -r -f decompressed; mkdir decompressed; mv *-d.dcm decompressed")
+        
+# decompress_file('/data/FetalLung/FetalLungNormal/LC20150615260/COR SSFSExr BODY/IM-0080-0006.dcm')
+pprint(unprocessed)
