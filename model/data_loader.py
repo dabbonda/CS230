4875815421a5ed544cf8e97db813280c50e718ad
David Xue <dxue@stanford.edu>
2018-03-17 14:47:42 -0700

    Updated data_loader to handle same z (apply rotations and resize)

--- model/data_loader.py
+++ model/data_loader.py
@@ -60,6 +60,15 @@ class FETALDataset(Dataset):
         raw_image = np.load(self.filenames[idx])    # load numpy array from .npy file
         raw_image = raw_image * (255.0 / raw_image.max()) if raw_image.max() != 0 else raw_image
 
+        image3d = []
+        for i in np.arange(raw_image.shape[0]):
+            image = Image.fromarray(raw_image[i,:,:])          # PIL image
+            image = image.resize((64, 64), Image.BILINEAR)
+            image = self.transform(image)
+            image3d.append(image)
+        image = torch.stack(image3d, 0)
+        print(image.size())
+
         # image = Image.fromarray(raw_image)          # PIL image
         # image = image.resize((64, 64), Image.BILINEAR)
         # image = self.transform(image)
